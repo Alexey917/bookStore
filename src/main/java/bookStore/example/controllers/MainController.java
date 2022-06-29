e380b9facb0879835d5c3649bf8cc4fd49500b29
Alexey <62547260+Alexey917@users.noreply.github.com>
2022-06-29 17:24:40 +0300

    Добавил не работающий переход на  станицу с подробным описанием

--- src/main/java/bookStore/example/controllers/MainController.java
+++ src/main/java/bookStore/example/controllers/MainController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
@@ -22,7 +26,15 @@ public class MainController {
         return "home";
     }
 
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public String book(@PathVariable("id") Long Id, Model model) {
 
+        Book books = BookRepository.getFirstById(Id);
+
+        model.addAttribute("book", books);
+
+        return "book";
+    }
 
 
 
--- src/main/java/bookStore/example/repo/BookRepository.java
+++ src/main/java/bookStore/example/repo/BookRepository.java
@@ -4,4 +4,5 @@ import bookStore.example.models.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book,Long> {
+    Book getFirstById(Long id);
 }
